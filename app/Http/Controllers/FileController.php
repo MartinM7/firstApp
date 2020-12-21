2d1deb27a3abc2de94977502505c8d3e2564587c
martin <martin@gmx.at>
2020-12-21 16:16:27 +0100

    media database finished

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Media;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -9,6 +10,7 @@ class FileController extends Controller
 {
     public function index()
     {
+
         $files = Storage::disk('local')->listcontents('/mnt', true);
 
         usort($files, function ($first, $second) {
@@ -18,6 +20,21 @@ class FileController extends Controller
         $files = array_filter($files, function ($file){
             return $file['type'] === 'file' && $file['extension'] !== 'nfo';
         });
+
+        foreach ($files as $file){
+            Media::firstOrCreate(
+                ['filename' => $file['filename']],
+                [
+                    'user_id' => auth()->id(),
+                    'type' => $file['type'],
+                    'path' => $file['path'],
+                    'size' => $file['size'],
+                    'basename' => $file['basename'],
+                    'extension' => $file['extension'],
+                    'filename' => $file['filename']
+                ]
+            );
+        }
 
         return view('files.index', [
             'files' => $files
---
+++ database/migrations/2020_12_21_133940_create_media_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned()->default(1);
+            $table->string('type');
+            $table->text('path');
+            $table->bigInteger('size');
+            $table->string('basename');
+            $table->string('extension');
+            $table->string('filename')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
